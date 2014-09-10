a797cf9b85326d6b8631c1ae41c5139e6d971459
Yochay Kiriaty <yochay@gmail.com>
2014-09-10 16:07:38 -0700

    Add new webjob via VS

---
+++ WebJob1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WebJob1
+{
+    // To learn more about Microsoft Azure WebJobs, please see http://go.microsoft.com/fwlink/?LinkID=401557
+    class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                //System.Diagnostics.Trace.TraceInformation
+                System.Diagnostics.Trace.WriteLine("teched New Zeland");
+                Console.WriteLine("teched New Zeland");; 
+                System.Threading.Thread.Sleep(3750);
+            }
+        }
+    }
+}
