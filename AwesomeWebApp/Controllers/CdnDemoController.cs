ae571494c80970733bad306f4f98392c52aa1eb8
Yochay Kiriaty <yochay@gmail.com>
2014-12-16 00:59:38 -0800

    Adding CDN demo page to show Azure CDN in action

---
+++ AwesomeWebApp/Controllers/CdnDemoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AwesomeWebApp.Controllers
+{
+    public class CdnDemoController : Controller
+    {
+        // GET: CdnDemo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
