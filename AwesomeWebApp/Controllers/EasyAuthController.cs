e8dd0fab785648cb0163029b156e2e254f22e1c7
Yochay Kiriaty <yochay@gmail.com>
2015-02-20 16:14:31 -0800

    add easy-auth, clean up webconfig, fix CDN broken origan id

---
+++ AwesomeWebApp/Controllers/EasyAuthController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AwesomeWebApp.Controllers
+{
+    public class EasyAuthController : Controller
+    {
+        // GET: EasyAuth
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
