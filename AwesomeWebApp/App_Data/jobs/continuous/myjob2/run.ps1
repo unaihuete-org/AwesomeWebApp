48e4a0975873911fe7fdc528e9ccada242f68163
Cory Fowler <cfowler@microsoft.com>
2015-10-26 17:38:12 -0700

    added Tests, Web Job Project, cleaned up code a bit

--- AwesomeWebApp/App_Data/jobs/continuous/myjob2/run.ps1
+++
@@ -1,6 +0,0 @@
-echo Starting work
-
-while ($true) {
-  echo "We be working in London"
-  Start-Sleep -s 8
-}
--- AwesomeWebApp/App_Data/jobs/continuous/myjob23/run.ps1
+++
@@ -1,6 +0,0 @@
-echo Starting work
-
-while ($true) {
-  echo "We be working"
-  Start-Sleep -s 5
-}
